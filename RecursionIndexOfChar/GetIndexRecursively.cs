5d3b8f7e3db03c9f3a612b7e27cd1b53bf933631
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-01-10 19:40:27 +0300

    Getting a Character Index (Recursion)

--- RecursionIndexOfChar/GetIndexRecursively.cs
+++ RecursionIndexOfChar/GetIndexRecursively.cs
@@ -6,16 +6,65 @@ namespace RecursionIndexOfChar
     {
         public static int GetIndexOfChar(string str, char value)
         {
-            // TODO #1. Analyze the implementation of "GetLastIndexOfChar(string, char)" method, and implement the method using recursion.
-            // The method should return the position of a "value" char in the "str" string. If "str" string has no "value" char then returns -1.
-            throw new NotImplementedException();
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return -1;
+            }
+
+            if (str[0] == value)
+            {
+                return 0;
+            }
+
+            int index = GetIndexOfChar(str[1..], value);
+            return index == -1 ? -1 : 1 + index;
         }
 
         public static int GetIndexOfChar(string str, char value, int startIndex, int count)
         {
-            // TODO #2. Analyze the implementation of "GetLastIndexOfChar(string, char, int, int)" method, and implement the method using recursion.
-            // The method should return the position of a "value" char in the "str" string. If "str" string has no "value" char then returns -1.
-            throw new NotImplementedException();
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (string.IsNullOrEmpty(str) || count == 0)
+            {
+                return -1;
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex >= str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater or equals str.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (startIndex + count > str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > str.Length");
+            }
+
+            if (str[startIndex] == value)
+            {
+                return startIndex;
+            }
+
+            int index = GetIndexOfChar(str[1..], value, startIndex, count - 1);
+
+            return index == -1 ? -1 : 1 + index;
         }
     }
 }
